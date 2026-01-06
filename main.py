943604080b122156d1a9ff890395008233a3a207
aozodbek2197 <aozodbek2197@gmail.com>
2026-01-06 14:51:18 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# # 6-misol
+# nums = [1, 2, 3, 4, 5, 7]
+# result = list(filter(lambda x: x % 3 == 1, nums))
+# # 7-misol
+# nums = [3, 6, 8]
+# result = list(map(lambda x: (x*2) % 10 if x*2 > 10 else x*2, nums))
+# # 8-misol
+# words = ["apple", "banana"]
+# result = list(map(lambda x: x.capitalize(), words))
+# # 9-misol
+# a = [2, 3, 4]
+# b = [1, 2, 3]
+# result = list(map(lambda x, y: x * y, a, b))
+# # 10-misol
+# nums = [-3, -1, 0, 2, 4]
+# result = list(filter(lambda x: x < 0, nums))
